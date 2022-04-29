4f3bb887d481816c0ffc3f6a3736a51a16e99fde
olsenbt <bennettolsen@yahoo.com>
2022-04-29 12:14:45 -0700

    start map1

---
+++ js/main.js
@@ -0,0 +1,94 @@
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    projection: 'albers',
+    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    zoom: 3.5, // starting zoom
+    center: [-97, 40] // starting position [lng, lat]
+});
+
+const grades = [10000, 20000, 100000],
+    colors = ['rgb(208,209,230)', 'rgb(120,120,0)', 'rgb(253,0,0)'],
+    radii = [5, 10, 15];
+
+
+map.on('load', () => {
+
+    map.addSource('covid', {
+        type: 'geojson',
+        data: 'assets/us-covid-2020-counts.geojson'
+    });
+
+    map.addLayer({
+            'id': 'covid-point',
+            'type': 'circle',
+            'source': 'covid',
+            'minzoom': 2,
+            'paint': {
+                // increase the radii of the circle as the zoom level and dbh value increases
+                'circle-radius': {
+                    'property': 'cases',
+                    'stops': [
+                        [{
+                            zoom: 5,
+                            value: grades[0]
+                        }, radii[0]],
+                        [{
+                            zoom: 5,
+                            value: grades[1]
+                        }, radii[1]],
+                        [{
+                            zoom: 5,
+                            value: grades[2]
+                        }, radii[2]]
+                    ]
+                },
+                'circle-color': {
+                    'property': 'cases',
+                    'stops': [
+                        [grades[0], colors[0]],
+                        [grades[1], colors[1]],
+                        [grades[2], colors[2]]
+                    ]
+                },
+                'circle-stroke-color': 'white',
+                'circle-stroke-width': 1,
+                'circle-opacity': 0.6
+            }
+        },
+        'waterway-label'
+    );
+
+    // click on tree to view magnitude in a popup
+    map.on('click', 'covid-point', (event) => {
+        new mapboxgl.Popup()
+            .setLngLat(event.features[0].geometry.coordinates)
+            .setHTML(`<strong>Cases:</strong> ${event.features[0].properties.cases}`)
+            .addTo(map);
+    });
+
+});
+
+// create legend
+const legend = document.getElementById('legend');
+
+//set up legend grades and labels
+var labels = ['<strong>Cases</strong>'], vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < grades.length; i++) {
+vbreak = grades[i];
+// you need to manually adjust the radius of each dot on the legend 
+// in order to make sure the legend can be properly referred to the dot on the map.
+dot_radii = 2 * radii[i];
+labels.push(
+    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
+    'px; height: ' +
+    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
+    '</span></p>');
+
+}
+const source =
+'<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
+
+legend.innerHTML = labels.join('') + source;
